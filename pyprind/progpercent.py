a8fd701ceb226a59499eada70eccbe31c36d7205
Chris Mutel <cmutel@Chriss-iMac.psi.ch>
2015-05-08 13:07:25 +0200

    get_foo is deprecated in favour of foo

--- pyprind/progpercent.py
+++ pyprind/progpercent.py
@@ -37,11 +37,11 @@ class ProgPercent(Prog):
         self._print()
         if monitor:
             try:
+                self.process.cpu_percent()
+                self.process.memory_percent()
+            except AttributeError: # old version of psutil
                 self.process.get_cpu_percent()
                 self.process.get_memory_percent()
-            except AttributeError: # old version of psutil
-                cpu_total = self.process.cpu_percent()
-                mem_total = self.process.memory_percent()   
 
 
     def _print(self):
