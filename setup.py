15012640361da6fcc0914f634db3fd7e8e6a738d
rasbt <se.raschka@me.com>
2015-07-21 10:02:46 -0400

    requirements fix

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(name='PyPrind',
       version='2.9.3',
@@ -35,11 +35,8 @@ object that let you track the progress of a loop structure or other iterative co
 Typical applications include the processing of large data sets to provide an intuitive estimate
 at runtime about the progress of the computation.
 
-For more details and examples please see the package documentation.
-
-A short video demonstration of the progress tracking can be found on YouTube:
-http://youtu.be/iWjSGe89Pvs
-
+For more details and examples please see the package documentation
+https://github.com/rasbt/pyprind/blob/master/README.md.
 
 Contact
 =============
